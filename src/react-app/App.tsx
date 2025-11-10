f3dbfccf24151b15ccb7682ec40ba8f1619c801b
xinzi1229 <506307050@qq.com>
2025-11-10 09:42:15 +0800

    1

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,11 +1,19 @@
 // src/App.tsx
+
 import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
+import honoLogo from "./assets/hono.svg";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("unknown");
+
   return (
     <>
-      <p>Hello World</p>
+    <p>Hello</p>
     </>
   );
 }
